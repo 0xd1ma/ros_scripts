d708d137d03568d5f141e296f62d1622f3523813
Демидов Дмитрий <demidov.d@starline.ru>
2017-12-15 18:45:33 +0300

    вставлено из лога антона команды

--- test.sh
+++ test.sh
@@ -4,9 +4,50 @@ MY_PATH=$(readlink -f $(dirname $0))
 echo "TOPCON setup"
 stty -F /dev/ttyUSB0 115200
 sleep 0.1
+
+echo -en "%%set,/par/pos/dion/mode,smooth\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/pos/dion/extrap,300\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/pos/dion/insextrap,on\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/pos/minsnr,30\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%PDOP%set,/par/pos/pdop,6.0\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,pos/dion/satnum,6\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%aims%set,lock/aims,3\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/dev/ser/d/imode,echo\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/dev/ser/d/echo,/dev/vp/f\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/dev/ser/d/rate,38400\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/kf/ahrs,{15,2.0}\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/kf/ahrs,{16,400.0}\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/kf/odom/use,on\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%SCALE%print,/par/mc/tp/kf/odom/scale\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "#%%set,/par/mc/tp/kf/spoffs,{1.03,0.52,-1.82}\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%spoffs%set,/par/mc/tp/kf/spoffs,{0.85,0.00,-1.35}\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/kf/odom/offs,{0,0,0}\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/save,on\r" > /dev/ttyUSB0
+sleep 0.05
+echo -en "%%set,/par/mc/tp/kf/reset,on\r" > /dev/ttyUSB0
+sleep 0.05
+
 echo -en "em,,nmea/GGA:0.1\r" > /dev/ttyUSB0
-sleep 0.1
-echo "ROS start"
+sleep 0.05
+
+echo "TOPCON is started"
 roslaunch ./nmea_navsat.launch
 
 
