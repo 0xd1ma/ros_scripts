00bc2ce0cbb0986b40c55ead057b02b5e120158b
Демидов Дмитрий <demidov.d@starline.ru>
2018-02-26 15:59:18 +0300

    много чего. потерян трек событий

--- topcon_roslaunch.sh
+++ topcon_roslaunch.sh
@@ -1,51 +1,64 @@
 #! /bin/bash
 clear
 
-MY_PATH=$(readlink -f $(dirname $0))
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && continue
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && continue
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
 
+echo " "
 echo "TOPCON setup"
+echo " "
 
-stty -F /dev/ttyUSB0 115200
+TOPCON_NAME="/dev/ttyUSB0"
+
+stty -F $TOPCON_NAME 115200
 sleep 0.1
 
-echo -en "%%set,/par/pos/dion/mode,smooth\r" > /dev/ttyUSB0
+echo -en "%%set,/par/pos/dion/mode,smooth\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/pos/dion/extrap,300\r" > /dev/ttyUSB0
+echo -en "%%set,/par/pos/dion/extrap,300\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/pos/dion/insextrap,on\r" > /dev/ttyUSB0
+echo -en "%%set,/par/pos/dion/insextrap,on\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/pos/minsnr,30\r" > /dev/ttyUSB0
+echo -en "%%set,/par/pos/minsnr,30\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%PDOP%set,/par/pos/pdop,6.0\r" > /dev/ttyUSB0
+echo -en "%PDOP%set,/par/pos/pdop,6.0\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,pos/dion/satnum,6\r" > /dev/ttyUSB0
+echo -en "%%set,pos/dion/satnum,6\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%aims%set,lock/aims,3\r" > /dev/ttyUSB0
+echo -en "%aims%set,lock/aims,3\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/dev/ser/d/imode,echo\r" > /dev/ttyUSB0
+echo -en "%%set,/par/dev/ser/d/imode,echo\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/dev/ser/d/echo,/dev/vp/f\r" > /dev/ttyUSB0
+echo -en "%%set,/par/dev/ser/d/echo,/dev/vp/f\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/dev/ser/d/rate,38400\r" > /dev/ttyUSB0
+echo -en "%%set,/par/dev/ser/d/rate,38400\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/kf/ahrs,{15,2.0}\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/kf/ahrs,{15,2.0}\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/kf/ahrs,{16,400.0}\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/kf/ahrs,{16,400.0}\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/kf/odom/use,on\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/kf/odom/use,on\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%SCALE%print,/par/mc/tp/kf/odom/scale\r" > /dev/ttyUSB0
+echo -en "%SCALE%print,/par/mc/tp/kf/odom/scale\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%spoffs%set,/par/mc/tp/kf/spoffs,{0.85,0.00,-1.35}\r" > /dev/ttyUSB0
+echo -en "%spoffs%set,/par/mc/tp/kf/spoffs,{0.85,0.00,-1.35}\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/kf/odom/offs,{0,0,0}\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/kf/odom/offs,{0,0,0}\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/save,on\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/save,on\r" > $TOPCON_NAME
 sleep 0.05
-echo -en "%%set,/par/mc/tp/kf/reset,on\r" > /dev/ttyUSB0
+echo -en "%%set,/par/mc/tp/kf/reset,on\r" > $TOPCON_NAME
 sleep 0.05
 
-echo -en "em,,nmea/GGA:0.1\r" > /dev/ttyUSB0
+echo -en "em,,nmea/GGA:0.1\r" > $TOPCON_NAME
 sleep 0.05
 
 echo "TOPCON is started"
